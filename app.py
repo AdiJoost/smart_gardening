43c013cef6864e9268841dea2b1b5831bd6e9437
Adi Joost <adi.joost@gmail.com>
2022-04-23 14:18:10 +0200

    commented all lines with RPi module

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask
-from pump import Pump
+#when run on pi, uncomment all lines run with Pump module
+#from pump import Pump
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def home():
-    pump_21 = Pump(21)
-    pump_20 = Pump (20)
-    pump_21.activate_pump(5)
-    pump_20.activate_pump(5)
     return "Hello world"
 
 
