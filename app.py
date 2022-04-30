33654aa33cd9676638fb5bbb93f864a4d5eb1f42
Adi Joost <adi.joost@gmail.com>
2022-04-30 19:01:29 +0200

    added tuple to enter all used pins, made comments to be able to run on pc

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_restful import Api
 from db import db
-#when run on pi, uncomment all lines run with Pump module
-#from pump import Pump
+#when run on Pi -make sure to uncomment the RPI.GPIO lib in Pump-class
+from pump import Pump
 
 app = Flask(__name__)
+pin_list = (20, 21, 26, 19, 13, 6, 5, 12)
+all_pumps = []
 
 @app.before_first_request
 def create_table():
@@ -12,9 +14,15 @@ def create_table():
 
 @app.route('/', methods=['GET'])
 def home():
+    for my_pump in all_pumps:
+        my_pump.activate_pump(3)
     return "Hello world"
 
 
 if __name__ == "__main__":
+    for pin in pin_list:
+        print(f"Going to add pump_pin {pin}")
+        all_pumps.append(Pump(pin))
+        print("Pin added")
     db.init_app(app)
     app.run(port=5000, host="0.0.0.0", debug=True)
--- pump.py
+++ pump.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
 
 class Pump():
@@ -12,26 +12,31 @@ class Pump():
         self.set_pin()
         
     def set_pin(self):
-        GPIO.setup(self.pump_pin, GPIO.OUT)
+#        GPIO.setup(self.pump_pin, GPIO.OUT)
+        return (f"Pin {self.pump_pin} is set")
     
     def pull_up(self):
-        GPIO.output(self.pump_pin, 1)
+#        GPIO.output(self.pump_pin, 1)
+        return (f"Pin {self.pump_pin} is high")
         
     def pull_down(self):
-        GPIO.output(self.pump_pin, 0)
+#        GPIO.output(self.pump_pin, 0)
+        return (f"Pin {self.pump_pin} is low")
         
     def activate_pump(self, seconds: int) -> str:
         self.pull_up()
         time.sleep(seconds)
         self.pull_down()
-        return "pump has shot"
+        return f"pump {self.pump_pin} has shot"
         
         
     @classmethod
     def setup_board(cls):
-        GPIO.setmode(GPIO.BCM)
+ #       GPIO.setmode(GPIO.BCM)
+        return "GPIO Board is setup in BCM mode"
     
     @classmethod
     def shut_board():
-        GPIO.cleanup()
+#        GPIO.cleanup()
+        return "GPIO Board is clean"
     
