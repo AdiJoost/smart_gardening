9a61a5c3db8902217d6b636fde5ac6d39970fadf
Adi Joost <adi.joost@gmail.com>
2022-05-15 11:51:26 +0200

    logger got function

--- log/logger.py
+++ log/logger.py
@@ -2,6 +2,22 @@
 """
 Created on Sun May 15 11:28:36 2022
 
-@author: JJ
+@author: AdiJo
 """
+import os.path
+from datetime import date
 
+def log_entry(prefix: str, message: str, file="main_log.txt"):
+    entry = f"[{prefix}] {message}"
+    if not os.path.exists(file):
+        with open(file, "w", encoding=("UTF-8")) as f:
+            f.write("Log-File for smart-gardening-system. Created:"\
+                    f" {date.today().isoformat()}\n\n")
+    with open(file, "a", encoding=("UTF-8")) as f:
+        f.write(f"{entry} -- {date.today().isoformat()}\n")
+        
+        
+
+
+log_entry("logger", "I just try to make an entry")
+log_entry("cdlogger", "I just try to make an other entry")
